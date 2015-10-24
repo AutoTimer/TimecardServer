3e92aa48b45eb3b10366c70d339c82016a8dda9a
Aparna.Radhakrishna <aparna.radhakrishna@autotrader.co.uk>
2015-10-24 14:53:23 +0100

    using filer writer service in the controller

--- src/main/java/controllers/ResultController.java
+++ src/main/java/controllers/ResultController.java
@@ -1,23 +1,28 @@
 package controllers;
 
 import model.Result;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
+import service.FileWriterService;
 
 @RestController
+@EnableAutoConfiguration
+@RequestMapping("/result")
 public class ResultController {
 
-    @RequestMapping(value="/result", method= RequestMethod.POST)
-    public List<Result> saveTime( @RequestBody Result result) {
-        List<Result> results = new ArrayList<>();
-        results.add(result);
-        return results;
+    @RequestMapping(method= RequestMethod.POST, consumes="application/json")
+    public void saveTime( @RequestBody Result result) {
+        FileWriterService fileWriterService = new FileWriterService();
+        fileWriterService.appendStringToFile(result);
     }
 
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ResultController.class, args);
+    }
 
 }
--- src/main/java/model/Result.java
+++ src/main/java/model/Result.java
@@ -6,13 +6,36 @@ public class Result {
     private String driverName;
     private String carNumber;
     private String layout;
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public void setLayout(String layout) {
+        this.layout = layout;
+    }
+
+    public void setTimeTaken(String timeTaken) {
+        this.timeTaken = timeTaken;
+    }
+
+    public void setRunTime(String runTime) {
+        this.runTime = runTime;
+    }
+
     private String timeTaken;
     private String runTime;
 
     public Result() {
+
     }
 
     public Result(String driverName, String carNumber, String layout, String timeTaken, String runTime) {
+
         this.driverName = driverName;
         this.carNumber = carNumber;
         this.layout = layout;
@@ -20,5 +43,23 @@ public class Result {
         this.runTime = runTime;
     }
 
+    public String getDriverName() {
+        return driverName;
+    }
 
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public String getLayout() {
+        return layout;
+    }
+
+    public String getTimeTaken() {
+        return timeTaken;
+    }
+
+    public String getRunTime() {
+        return runTime;
+    }
 }
