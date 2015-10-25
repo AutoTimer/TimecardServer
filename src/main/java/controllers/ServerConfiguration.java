c20d1b5ed8dd544c9d3dff76dac5661f80072710
Alec Tunbridge <alec.tunbridge@gmail.com>
2015-10-25 09:32:16 +0000

    Added Angular...maybe

--- src/main/java/controllers/ServerConfiguration.java
+++ src/main/java/controllers/ServerConfiguration.java
@@ -2,6 +2,15 @@ package controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
 
 @SpringBootApplication
 public class ServerConfiguration {
@@ -9,5 +18,4 @@ public class ServerConfiguration {
     public static void main(String[] args) {
         SpringApplication.run(ServerConfiguration.class, args);
     }
-
 }
