b1608f023bab9f08663d42ceac7b4441dd791cff
conor toner <conor.toner@autotrader.co.uk>
2015-10-25 11:31:53 +0000

    added the simple angular display page

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,1 +1,4 @@
-
+angular.module('displayResults', []);
+angular.module('app', [
+    'displayResults'
+]);
--- src/main/webapp/js/resultsService.js
+++ src/main/webapp/js/resultsService.js
@@ -1,1 +1,25 @@
+(function() {
+    'use strict';
+    angular.module('displayResults').service('resultsService', resultsService);
+
+    resultsService.$inject = ['$q', '$http'];
+
+    function resultsService($q, $http) {
+
+        return {
+            getSummary:function(){
+                var deferred = $q.defer();
+                var getUrl = "http://localhost:8080/results-summary";
+                $http.get(getUrl)
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    }).error(function (data, status) {
+                        deferred.reject(data+":"+status);
+                    });
+                return deferred.promise;
+            }
+        }
+
+    }
+}());
 
