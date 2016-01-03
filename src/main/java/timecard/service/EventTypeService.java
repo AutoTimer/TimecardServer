308767a447a5b6910b4277e63d5d100177da0d41
Alec Tunbridge <alec.tunbridge@gmail.com>
2016-01-03 22:16:40 +0000

    Allow times to be dropped #1 - added EventType.

--- src/main/java/timecard/service/EventTypeService.java
+++ src/main/java/timecard/service/EventTypeService.java
@@ -1,14 +1,24 @@
 package timecard.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import timecard.model.EventType;
+
+import java.util.List;
 
 @Service
 public class EventTypeService {
 
-    public EventTypeService() {
+    private final FileService fileService;
+
+    @Autowired
+    public EventTypeService(FileService fileService) {
+        this.fileService = fileService;
     }
 
-    public boolean dropsTimes(String eventType) {
-        return true;
+    public boolean dropsTimes(String name) {
+        List<EventType> eventTypes = fileService.readEntitiesFromFile(EventType.class);
+        EventType event = eventTypes.stream().filter(x -> x.getName().equals(name)).findFirst().get();
+        return event.isDropsTimes();
     }
 }
